8bf58e0830f19a6f5cb97a465fe0faed8b075b3f
Erskine Williams <github@archerwilliams.com>
2024-12-04 12:16:39 -0800

    shenanigans

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-poetry run streamlit run llm_eval/ui.py --server.port 8501 --server.address 0.0.0.0 &
-poetry run uvicorn llm_eval.api:app --host 0.0.0.0 --port 8000 
+poetry run uvicorn llm_eval.api:app --host 0.0.0.0 --port 8000 & 
+poetry run streamlit run llm_eval/ui.py --server.port 8501 --server.address 0.0.0.0
