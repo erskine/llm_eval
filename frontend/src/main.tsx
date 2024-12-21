9931c7906ce884d1d2b8acedd4aadb7cde7b3e49
Erskine Williams <github@archerwilliams.com>
2024-12-21 13:59:25 -0800

    removing tanstack query toosl

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -11,7 +10,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 )
